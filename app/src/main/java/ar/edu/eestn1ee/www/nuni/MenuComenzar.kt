2c5e7cb0e861862b9924fe8ce532fb8ae123b8e4
Diego Cervetti <diego@eestn1ee.edu.ar>
2017-05-26 13:51:06 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/eestn1ee/www/nuni/MenuComenzar.kt
@@ -0,0 +1,12 @@
+package ar.edu.eestn1ee.www.nuni
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuComenzar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_comenzar)
+    }
+}
